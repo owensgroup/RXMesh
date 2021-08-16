9b3412dcc9f797d267abdef5dfc07995915bfc0b
Ahmed <eng.a.hassen@gmail.com>
2021-08-15 20:47:10 -0400

    MCF and Geodesic scripts

---
+++ apps/Geodesic/benchmark.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "This script re-generates RXMesh data in Figure 8(b) in the paper."
+echo "Please make sure to first compile the source code and then enter the input OBJ files directory."
+read -p "OBJ files directory (no trailing slash): " input_dir
+
+echo "Input directory= $input_dir"
+exe="../../build/bin/Geodesic"
+
+if [ ! -f $exe ]; then 
+	echo "The code has not been compiled. Please compile Geodesic and retry!"
+	exit 1
+fi
+
+num_run=10
+device_id=0
+
+for file in $input_dir/*.obj; do 	 
+    if [ -f "$file" ]; then
+		echo $exe -p -input "$file" -device_id $device_id
+             $exe -p -input "$file" -device_id $device_id
+    fi 
+done
