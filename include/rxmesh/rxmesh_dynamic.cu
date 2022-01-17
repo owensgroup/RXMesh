c565aa20f2a72b8cdd31e41a0f46c71f2df001a9
Ahmed Mahmoud <ahmahmoud@ucdavis.edu>
2022-01-16 20:20:54 -0500

    WIP check uniqueness

--- include/rxmesh/rxmesh_dynamic.cu
+++ include/rxmesh/rxmesh_dynamic.cu
@@ -26,11 +26,19 @@ __global__ static void calc_num_elements(const Context context,
     }
 }
 
+__global__ static void check_uniqueness(const Context context,
+                                        uint32_t*     d_check)
+{
+}
+
 }  // namespace detail
 
 bool RXMeshDynamic::validate()
 {
     CUDA_ERROR(cudaDeviceSynchronize());
+
+    // check that the sum of owned vertices, edges, and faces per patch is equal
+    // to the number of vertices, edges, and faces respectively.
 
     auto check_num_mesh_elements = [&]() -> bool {
         uint32_t *d_sum_num_vertices, *d_sum_num_edges, *d_sum_num_faces;
@@ -96,7 +104,44 @@ bool RXMeshDynamic::validate()
         }
     };
 
+    // check that each edge is composed of two unique vertices and each face is
+    // composed of three unique edges
+    auto check_uniqueness = [&]() -> bool {
+        uint32_t num_patches;
+        CUDA_ERROR(cudaMemcpy(&num_patches,
+                              m_rxmesh_context.m_num_patches,
+                              sizeof(uint32_t),
+                              cudaMemcpyDeviceToHost));
+
+        uint32_t* d_check;
+        CUDA_ERROR(cudaMalloc((void**)&d_check, sizeof(uint32_t)));
+        CUDA_ERROR(cudaMemset(d_check, 0, sizeof(uint32_t)));
+
+        const uint32_t block_size   = 256;
+        const uint32_t grid_size    = num_patches;
+        const uint32_t dynamic_smem = (3 * this->m_max_faces_per_patch +
+                                       2 * this->m_max_edges_per_patch) *
+                                      sizeof(uint16_t);
+
+        detail::check_uniqueness<<<grid_size, block_size, dynamic_smem>>>(
+            m_rxmesh_context, d_check);
+
+        uint32_t h_check;
+        CUDA_ERROR(cudaMemcpy(
+            &h_check, d_check, sizeof(uint32_t), cudaMemcpyDeviceToHost));
+
+        if (h_check != 0) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+
     if (!check_num_mesh_elements()) {
+        return false;
+    }
+
+    if (!check_uniqueness()) {
         return false;
     }
 
