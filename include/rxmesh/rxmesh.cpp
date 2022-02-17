60f692b2be425c10c5c6cf96b206e3e25a075556
Ahmed Mahmoud <ahmahmoud@ucdavis.edu>
2022-02-16 23:30:21 -0500

    minor fix

--- include/rxmesh/rxmesh.cpp
+++ include/rxmesh/rxmesh.cpp
@@ -625,9 +625,9 @@ void RXMesh::build_device()
             [](uint64_t*& d_mask, uint64_t*& h_mask, uint32_t num_owned) {
                 size_t num_bytes = DIVIDE_UP(num_owned, 64) * sizeof(uint64_t);
                 h_mask           = (uint64_t*)malloc(num_bytes);
-                std::memset(h_mask, 1, num_bytes);
+                std::memset(h_mask, 0xFF, num_bytes);
                 CUDA_ERROR(cudaMalloc((void**)&d_mask, num_bytes));
-                CUDA_ERROR(cudaMemset(d_mask, 1, num_bytes));
+                CUDA_ERROR(cudaMemset(d_mask, 0xFF, num_bytes));
             };
 
         alloc_bitmask(
