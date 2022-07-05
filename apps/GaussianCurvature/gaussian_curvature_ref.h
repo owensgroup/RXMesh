f57c837c2b8cfc640e2e7ad9baf6589e0948eb45
Eric Yuan <116010276@link.cuhk.edu.cn>
2022-07-05 15:32:21 -0700

    format and name fix

--- apps/GaussianCurvature/gaussian_curvature_ref.h
+++ apps/GaussianCurvature/gaussian_curvature_ref.h
@@ -5,9 +5,10 @@
 const double PI = 3.1415926535897932384626433832795028841971693993751058209;
 
 template <typename T>
-inline void gaussian_curvature_ref(const std::vector<std::vector<uint32_t>>& Faces,
-                              const std::vector<std::vector<T>>&        Verts,
-                              std::vector<T>& gaussian_curvature)
+inline void gaussian_curvature_ref(
+    const std::vector<std::vector<uint32_t>>& Faces,
+    const std::vector<std::vector<T>>&        Verts,
+    std::vector<T>&                           gaussian_curvature)
 {
     uint32_t num_vertices = Verts.size();
     uint32_t num_faces    = Faces.size();
@@ -25,11 +26,11 @@ inline void gaussian_curvature_ref(const std::vector<std::vector<uint32_t>>& Fac
     T        fn[3];
 
     auto l2_norm_sq = [](const T ax0,
-                        const T ax1,
-                        const T ax2,
-                        const T bx0,
-                        const T bx1,
-                        const T bx2) {
+                         const T ax1,
+                         const T ax2,
+                         const T bx0,
+                         const T bx1,
+                         const T bx2) {
         // compute (xa0-xb0)*(xa0-xb0) + (xa1-xb1)*(xa1-xb1) +
         // (xa2-xb2)*(xa2-xb2)
         T x0 = ax0 - bx0;
@@ -38,36 +39,34 @@ inline void gaussian_curvature_ref(const std::vector<std::vector<uint32_t>>& Fac
         return (x0 * x0 + x1 * x1 + x2 * x2);
     };
 
-    auto cross_product_norm =
-        [](T xv1, T yv1, T zv1, T xv2, T yv2, T zv2) {
-            T xx = yv1 * zv2 - zv1 * yv2;
-            T yy = zv1 * xv2 - xv1 * zv2;
-            T zz = xv1 * yv2 - yv1 * xv2;
-            return sqrt(xx * xx + yy * yy + zz * zz); // l2 norm
-        };
+    auto cross_product_norm = [](T xv1, T yv1, T zv1, T xv2, T yv2, T zv2) {
+        T xx = yv1 * zv2 - zv1 * yv2;
+        T yy = zv1 * xv2 - xv1 * zv2;
+        T zz = xv1 * yv2 - yv1 * xv2;
+        return sqrt(xx * xx + yy * yy + zz * zz);  // l2 norm
+    };
 
-    auto dot_product = 
-        [](T xv1, T yv1, T zv1, T xv2, T yv2, T zv2) {
-            return xv1 * xv2 + yv1 * yv2 + zv1 * zv2;
-        };
+    auto dot_product = [](T xv1, T yv1, T zv1, T xv2, T yv2, T zv2) {
+        return xv1 * xv2 + yv1 * yv2 + zv1 * zv2;
+    };
 
     for (uint32_t f = 0; f < num_faces; ++f) {
-        v[0] = Faces[f][0];
-        v[1] = Faces[f][1];
-        v[2] = Faces[f][2];
+        v[0]       = Faces[f][0];
+        v[1]       = Faces[f][1];
+        v[2]       = Faces[f][2];
         bool is_ob = false;
 
         angle_sin = cross_product_norm(Verts[v[1]][0] - Verts[v[0]][0],
-                                        Verts[v[1]][1] - Verts[v[0]][1],
-                                        Verts[v[1]][2] - Verts[v[0]][2],
-                                        Verts[v[2]][0] - Verts[v[0]][0],
-                                        Verts[v[2]][1] - Verts[v[0]][1],
-                                        Verts[v[2]][2] - Verts[v[0]][2]);
+                                       Verts[v[1]][1] - Verts[v[0]][1],
+                                       Verts[v[1]][2] - Verts[v[0]][2],
+                                       Verts[v[2]][0] - Verts[v[0]][0],
+                                       Verts[v[2]][1] - Verts[v[0]][1],
+                                       Verts[v[2]][2] - Verts[v[0]][2]);
 
         for (uint32_t i = 0; i < 3; ++i) {
             uint32_t i1    = (i + 1) % 3;
             uint32_t i2    = (i + 2) % 3;
-            angle_cos[i] = dot_product(Verts[v[i1]][0] - Verts[v[i]][0],
+            angle_cos[i]   = dot_product(Verts[v[i1]][0] - Verts[v[i]][0],
                                        Verts[v[i1]][1] - Verts[v[i]][1],
                                        Verts[v[i1]][2] - Verts[v[i]][2],
                                        Verts[v[i2]][0] - Verts[v[i]][0],
@@ -79,13 +78,14 @@ inline void gaussian_curvature_ref(const std::vector<std::vector<uint32_t>>& Fac
                                         Verts[v[i1]][0],
                                         Verts[v[i1]][1],
                                         Verts[v[i1]][2]);
-            rads[i] = atan2(angle_sin, angle_cos[i]);
-            if (rads[i] > PI * 0.5) is_ob = true;
+            rads[i]        = atan2(angle_sin, angle_cos[i]);
+            if (rads[i] > PI * 0.5)
+                is_ob = true;
         }
 
         for (uint32_t i = 0; i < 3; ++i) {
-            uint32_t i1    = (i + 1) % 3;
-            uint32_t i2    = (i + 2) % 3;
+            uint32_t i1 = (i + 1) % 3;
+            uint32_t i2 = (i + 2) % 3;
 
             if (is_ob) {
                 if (rads[i] > PI * 0.5) {
@@ -95,16 +95,16 @@ inline void gaussian_curvature_ref(const std::vector<std::vector<uint32_t>>& Fac
                 }
             } else {
                 // veronoi region calculation
-                region_mixed[v[i]] += 0.125 * ( (edge_len_sq[i2]) * (angle_cos[i1] / angle_sin) 
-                                            + (edge_len_sq[i]) * (angle_cos[i2] / angle_sin) );
+                region_mixed[v[i]] +=
+                    0.125 * ((edge_len_sq[i2]) * (angle_cos[i1] / angle_sin) +
+                             (edge_len_sq[i]) * (angle_cos[i2] / angle_sin));
             }
 
             gaussian_curvature[v[i]] -= rads[i];
         }
     }
 
-    for (uint32_t n = 0; n < num_vertices; ++n)  {
+    for (uint32_t n = 0; n < num_vertices; ++n) {
         gaussian_curvature[n] = gaussian_curvature[n] / region_mixed[n];
     }
 }
- 
