8c66b43675d70514bd880bb3eb88947b0247545f
ahmed <ahmahmoud@ucdavis.edu>
2022-07-23 14:37:44 -0400

    LB hashtable benchmark

--- tests/RXMesh_test/test_lp_pair.h
+++ tests/RXMesh_test/test_lp_pair.h
@@ -65,3 +65,66 @@ TEST(RXMesh, LPHashTable)
 
     table.free();
 }
+
+TEST(RXMesh, DISABLED_BenchmarkLPHashTable)
+{
+    using namespace rxmesh;
+
+    std::cout << "size, cap, num_failed\n";
+    const float    load_factor = 0.8;
+    const uint32_t low_size    = 128;
+    const uint32_t high_size   = 2048;
+    const int      num_run     = 2E6;
+
+    auto random_with_bounds = [&](auto& vec, uint32_t high_val, uint32_t size) {
+        for (uint32_t i = 0; i < size; ++i) {
+            vec[i] = i % high_val;
+        }
+        random_shuffle(vec.data(), size);
+    };
+
+
+    for (uint32_t size = low_size; size <= high_size; size++) {
+
+        int      num_failed = 0;
+        uint32_t cap        = 0;
+
+        for (int t = 0; t < num_run; ++t) {
+            std::vector<uint16_t> local_id(size);
+            fill_with_random_numbers(local_id.data(), local_id.size());
+
+            std::vector<uint16_t> owner_local_id(size);
+            random_with_bounds(owner_local_id,
+                               1 << LPPair::LIDOwnerNumBits,
+                               owner_local_id.size());
+
+            std::vector<uint8_t> patch_id(size);
+            random_with_bounds(
+                patch_id, 1 << LPPair::PatchStashNumBits, patch_id.size());
+
+            std::vector<LPPair> pairs(size);
+            for (uint32_t i = 0; i < size; ++i) {
+                pairs[i] = LPPair(local_id[i], owner_local_id[i], patch_id[i]);
+            }
+
+            LPHashTable table(
+                static_cast<uint16_t>(static_cast<float>(size) / load_factor),
+                false);
+
+            bool fail = false;
+            for (auto& p : pairs) {
+                if (!table.insert(p, table.get_table())) {
+                    fail = true;
+                    break;
+                }
+            }
+            if (fail) {
+                num_failed++;
+            }
+
+            cap = table.get_capacity();
+            table.free();
+        }
+        std::cout << size << ", " << cap << ", " << num_failed << "\n";
+    }
+}
