32113adc8feaf07a060d9cd9a1b12cbf54ce7db3
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2022-12-26 14:35:37 -0500

    test conflicting edge flips on the ribbon

--- tests/RXMesh_test/test_dynamic.cuh
+++ tests/RXMesh_test/test_dynamic.cuh
@@ -35,6 +35,12 @@ __global__ static void edge_flip_kernel(rxmesh::Context                context,
                     cavity.add(eh);
                 }
             } else {
+                if (eh.unpack().second == 11 || eh.unpack().second == 10 ||
+                    eh.unpack().second == 358 || eh.unpack().second == 359 ||
+                    eh.unpack().second == 354 || eh.unpack().second == 356 ) {
+                    e_attr(eh) = 100;
+                    cavity.add(eh);
+                }
             }
         } else {
             if (!conflicting) {
@@ -131,7 +137,7 @@ TEST(RXMeshDynamic, Cavity)
     edge_flip_kernel<blockThreads><<<launch_box.blocks,
                                      launch_box.num_threads,
                                      launch_box.smem_bytes_dyn>>>(
-        rx.get_context(), *coords, *v_attr, *e_attr, *f_attr, false, true);
+        rx.get_context(), *coords, *v_attr, *e_attr, *f_attr, true, true);
 
     CUDA_ERROR(cudaDeviceSynchronize());
 
