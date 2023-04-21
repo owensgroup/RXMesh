11958ba933c7423261e28592227d9933d4cdeb26
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2023-04-21 18:02:05 -0400

    Update sanatize.cmd

--- scripts/sanatize.cmd
+++ scripts/sanatize.cmd
@@ -1,6 +1,6 @@
 @echo off
-REM --generate-coredump yes --kernel-regex kns=delaunay --launch-skip 6 
+REM --generate-coredump yes --kernel-regex kns=delaunay --launch-skip 6 --check-cache-control yes
 call compute-sanitizer  --log-file sanitize_memcheck.log --tool memcheck  --leak-check full ..\build\bin\Debug\RXMesh_test.exe --gtest_filter=RXMeshDynamic.Cavity
 call compute-sanitizer  --log-file sanitize_racecheck.log --tool racecheck  --racecheck-report analysis ..\build\bin\Debug\RXMesh_test.exe --gtest_filter=RXMeshDynamic.Cavity
-REM call compute-sanitizer  --log-file sanitize_initcheck.log --tool initcheck  ..\build\bin\Debug\DelaunayEdgeFlip.exe
+call compute-sanitizer  --log-file sanitize_initcheck.log --tool initcheck --track-unused-memory yes ..\build\bin\Debug\RXMesh_test.exe --gtest_filter=RXMeshDynamic.Cavity
 REM call compute-sanitizer  --log-file sanitize_synccheck.log --tool synccheck   ..\build\bin\Debug\DelaunayEdgeFlip.exe
