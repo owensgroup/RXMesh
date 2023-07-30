7f51e78caaf235127991f8c3e59e8ffa768c62cf
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2023-07-30 18:12:28 -0400

    Update sanatize.cmd

--- scripts/sanatize.cmd
+++ scripts/sanatize.cmd
@@ -1,10 +1,4 @@
 @echo off
-REM --generate-coredump yes --kernel-regex kns=delaunay --launch-skip 6 --check-cache-control yes
-::call compute-sanitizer  --log-file sanitize_memcheck.log --tool memcheck  --leak-check full ..\build\bin\Debug\RXMesh_test.exe --gtest_filter=RXMeshDynamic.RandomFlips
-call compute-sanitizer  --log-file sanitize_racecheck.log --tool racecheck  --racecheck-report analysis ..\build\bin\Debug\RXMesh_test.exe --gtest_filter=RXMeshDynamic.RandomFlips
+call compute-sanitizer  --log-file sanitize_memcheck.log --tool memcheck --check-cache-control yes --leak-check full ..\build\bin\Debug\DelaunayEdgeFlip.exe  -input ..\input\ear2.obj
+::call compute-sanitizer  --log-file sanitize_racecheck.log --tool racecheck  --racecheck-report analysis ..\build\bin\Debug\RXMesh_test.exe --gtest_filter=RXMeshDynamic.RandomFlips
 ::call compute-sanitizer  --log-file sanitize_initcheck.log --tool initcheck --track-unused-memory yes ..\build\bin\Debug\RXMesh_test.exe --gtest_filter=RXMeshDynamic.RandomFlips
-REM call compute-sanitizer  --log-file sanitize_synccheck.log --tool synccheck   ..\build\bin\Debug\DelaunayEdgeFlip.exe
-
-::call compute-sanitizer  --log-file sanitize_memcheck.log --tool memcheck  --leak-check full ..\build\bin\Debug\DelaunayEdgeFlip.exe
-::call compute-sanitizer  --log-file sanitize_racecheck.log --tool racecheck  --racecheck-report analysis ..\build\bin\Debug\DelaunayEdgeFlip.exe
-::call compute-sanitizer  --log-file sanitize_initcheck.log --tool initcheck --track-unused-memory yes ..\build\bin\Debug\DelaunayEdgeFlip.exe
