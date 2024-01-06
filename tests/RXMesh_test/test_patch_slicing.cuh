99e8afcf4c422d43a22bacbc324f3c27b2d2aab1
ahmed <ahmahmoud@ucdavis.edu>
2024-01-05 19:20:18 -0500

    Update test_patch_slicing.cuh

--- tests/RXMesh_test/test_patch_slicing.cuh
+++ tests/RXMesh_test/test_patch_slicing.cuh
@@ -3,11 +3,12 @@
 
 #include "rxmesh/rxmesh_dynamic.h"
 
-__global__ static void set_patch_should_slice(rxmesh::Context context)
+__global__ static void set_patch_should_slice(rxmesh::Context context, int p)
 {
     uint32_t tid = threadIdx.x + blockIdx.x * blockDim.x;
 
-    if (tid < context.m_num_patches[0]) {
+    // if (tid < context.m_num_patches[0]) {
+    if (tid == p) {
         context.m_patches_info[tid].should_slice = true;
     }
 }
@@ -17,8 +18,10 @@ TEST(RXMeshDynamic, PatchSlicing)
     using namespace rxmesh;
     cuda_query(rxmesh_args.device_id);
 
-    RXMeshDynamic rx(STRINGIFY(INPUT_DIR) "sphere3.obj",
-                     STRINGIFY(INPUT_DIR) "sphere3_patches");
+    RXMeshDynamic rx(rxmesh_args.obj_file_name,
+                     STRINGIFY(OUTPUT_DIR) +
+                         extract_file_name(rxmesh_args.obj_file_name) +
+                         "_patches");
 
     auto coords = rx.get_input_vertex_coordinates();
 
@@ -26,21 +29,30 @@ TEST(RXMeshDynamic, PatchSlicing)
     const uint32_t num_edges    = rx.get_num_edges();
     const uint32_t num_faces    = rx.get_num_faces();
 
+    EXPECT_TRUE(rx.validate());
+
 #if USE_POLYSCOPE
     rx.render_vertex_patch();
     rx.render_edge_patch();
     rx.render_face_patch();
-    rx.render_patch(0)->setEnabled(false);
-    rx.render_patch(1)->setEnabled(false);
+    // rx.render_patch(0)->setEnabled(false);
+    // polyscope::show();
 #endif
 
-    set_patch_should_slice<<<rx.get_num_patches(), 1>>>(rx.get_context());
+    RXMESH_INFO("Input mesh #Patches {}", rx.get_num_patches());
 
-    // rx.copy_patch_debug(0, *coords);
-    rx.slice_patches(*coords);
-    rx.cleanup();
+    // set_patch_should_slice<<<rx.get_num_patches(), 1>>>(rx.get_context());
+    //  rx.copy_patch_debug(0, *coords);
+    uint32_t num_pp = rx.get_num_patches();
+    for (uint32_t p = 0; p < num_pp; ++p) {
+        set_patch_should_slice<<<rx.get_num_patches(), 1>>>(rx.get_context(),
+                                                            p);
+        rx.slice_patches(*coords);
+        rx.cleanup();
+    }
 
     CUDA_ERROR(cudaDeviceSynchronize());
+    CUDA_ERROR(cudaGetLastError());
 
     rx.update_host();
     EXPECT_TRUE(rx.validate());
