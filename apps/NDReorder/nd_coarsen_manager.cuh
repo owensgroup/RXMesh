cadc114f323612f1a555c88b03caeef4b6c0cab6
Eric Yuan <116010276@link.cuhk.edu.cn>
2024-01-24 17:26:23 -0800

    shared mem tests

--- apps/NDReorder/nd_coarsen_manager.cuh
+++ apps/NDReorder/nd_coarsen_manager.cuh
@@ -18,8 +18,7 @@
 
 namespace rxmesh {
 
-// Structure of array design
-// use CSR like structure to represent the level
+
 template <uint32_t blockThreads>
 struct ALIGN(16) CoarsePatchinfo
 {
@@ -28,8 +27,9 @@ struct ALIGN(16) CoarsePatchinfo
           m_s_vwgt(nullptr),
           m_s_vdegree(nullptr),
           m_s_vadjewgt(nullptr),
-          m_mapping(nullptr),
-          m_unmapping(nullptr),
+          m_s_matching(nullptr),
+          m_s_mapping(nullptr),
+          m_s_unmapping(nullptr),
           m_s_ewgt(nullptr),
           m_s_num_vertices(nullptr),
           m_s_num_edges(nullptr)
@@ -39,90 +39,10 @@ struct ALIGN(16) CoarsePatchinfo
     __device__ __inline__ CoarsePatchinfo(
         cooperative_groups::thread_block& block,
         Context&                          context,
-        ShmemAllocator&                   shrd_alloc)
-    {
-        m_context = context;
+        ShmemAllocator&                   shrd_alloc);
 
-        __shared__ uint32_t s_patch_id;
-
-        // magic for s_patch_id
-        // get a patch
-        s_patch_id = m_context.m_patch_scheduler.pop();
-#ifdef PROCESS_SINGLE_PATCH
-        if (s_patch_id != current_p) {
-            s_patch_id = INVALID32;
-        }
-#endif
-
-
-        if (s_patch_id != INVALID32) {
-            if (m_context.m_patches_info[s_patch_id].patch_id == INVALID32) {
-                s_patch_id = INVALID32;
-            }
-        }
-
-        // try to lock the patch
-        if (s_patch_id != INVALID32) {
-            bool locked =
-                m_context.m_patches_info[s_patch_id].lock.acquire_lock(
-                    blockIdx.x);
-
-            if (!locked) {
-                // if we can not, we add it again to the queue
-                if (threadIdx.x == 0) {
-                    bool ret = m_context.m_patch_scheduler.push(s_patch_id);
-                    assert(ret);
-                }
-
-                // and signal other threads to also exit
-                s_patch_id = INVALID32;
-            }
-        }
-
-        if (s_patch_id != INVALID32) {
-            m_s_num_vertices[0] =
-                m_context.m_patches_info[s_patch_id].num_vertices[0];
-            m_s_num_edges[0] =
-                m_context.m_patches_info[s_patch_id].num_edges[0];
-        }
-
-        // block.sync();
-
-        if (s_patch_id == INVALID32) {
-            return;
-        }
-
-
-        m_patch_info = m_context.m_patches_info[s_patch_id];
-
-        // static shared memory for fixed size variables
-        __shared__ uint16_t counts[2];
-        m_s_num_vertices = counts + 0;
-        m_s_num_edges    = counts + 1;
-
-
-        // get the capacity for dynamic shared memory allocation
-        const uint16_t vert_cap = m_patch_info.vertices_capacity[0];
-        const uint16_t edge_cap = m_patch_info.edges_capacity[0];
-        const uint16_t face_cap = m_patch_info.faces_capacity[0];
-
-        // copy ev to shared memory
-        m_s_ev = shrd_alloc.alloc<uint16_t>(2 * edge_cap);
-        detail::load_async(block,
-                           reinterpret_cast<uint16_t*>(m_patch_info.ev),
-                           2 * m_s_num_edges[0],
-                           m_s_ev,
-                           false);
-
-        // alloc shared memory for all vertex attributes
-        m_s_vwgt     = shrd_alloc.alloc<uint16_t>(vert_cap);
-        m_s_vdegree  = shrd_alloc.alloc<uint16_t>(vert_cap);
-        m_s_vadjewgt = shrd_alloc.alloc<uint16_t>(vert_cap);
-
-        // alloc shared memory for edge attrobutes
-        m_s_ewgt = shrd_alloc.alloc<uint16_t>(edge_cap);
-
-        // block.sync();
+    __device__ __inline__ void init_array() {
+        
     }
 
     // The topology information: edge incident vertices and face incident
@@ -144,19 +64,123 @@ struct ALIGN(16) CoarsePatchinfo
     uint16_t* m_s_vdegree;
     uint16_t* m_s_vadjewgt;
 
-    // a private array owned by every vertex ?
-    uint16_t* m_mapping;
-    uint16_t* m_unmapping;
-
     // edge related attribute
     uint16_t* m_s_ewgt;
 
-    // indicates the current level
-    uint16_t* level_count;
+    // indicated the mapping relationship and unmapping relationship
+    uint16_t* m_s_mapping;
+    uint16_t* m_s_unmapping;
+
+    // tmp matching array
+    uint16_t* m_s_matching;
+
+    // indicates the number of levels we have, notice right now the level size of the capacity
+    uint16_t* m_s_level_count;
 
     // basic rxmesh info
     PatchInfo m_patch_info;
     Context   m_context;
 };
+
+
+
+
+template <uint32_t blockThreads>
+__device__ __inline__ CoarsePatchinfo<blockThreads>::CoarsePatchinfo(
+    cooperative_groups::thread_block& block,
+    Context&                          context,
+    ShmemAllocator&                   shrd_alloc)
+{
+
+    m_context = context;
+
+    __shared__ uint32_t s_patch_id;
+
+    // magic for s_patch_id
+    // get a patch
+    s_patch_id = m_context.m_patch_scheduler.pop();
+#ifdef PROCESS_SINGLE_PATCH
+    if (s_patch_id != current_p) {
+        s_patch_id = INVALID32;
+    }
+#endif
+
+
+    if (s_patch_id != INVALID32) {
+        if (m_context.m_patches_info[s_patch_id].patch_id == INVALID32) {
+            s_patch_id = INVALID32;
+        }
+    }
+
+    // try to lock the patch
+    if (s_patch_id != INVALID32) {
+        bool locked =
+            m_context.m_patches_info[s_patch_id].lock.acquire_lock(blockIdx.x);
+
+        if (!locked) {
+            // if we can not, we add it again to the queue
+            if (threadIdx.x == 0) {
+                bool ret = m_context.m_patch_scheduler.push(s_patch_id);
+                assert(ret);
+            }
+
+            // and signal other threads to also exit
+            s_patch_id = INVALID32;
+        }
+    }
+
+    if (s_patch_id != INVALID32) {
+        m_s_num_vertices[0] =
+            m_context.m_patches_info[s_patch_id].num_vertices[0];
+        m_s_num_edges[0] = m_context.m_patches_info[s_patch_id].num_edges[0];
+    }
+
+    block.sync();
+
+    if (s_patch_id == INVALID32) {
+        return;
+    }
+
+    m_patch_info = m_context.m_patches_info[s_patch_id];
+
+    // static shared memory for fixed size variables
+    __shared__ uint16_t counts[2];
+    m_s_num_vertices = counts + 0;
+    m_s_num_edges    = counts + 1;
+
+    __shared__ uint16_t level_count[1];
+    m_s_level_count = level_count;
+
+    // get the capacity for dynamic shared memory allocation
+    const uint16_t vert_cap = m_patch_info.vertices_capacity[0];
+    const uint16_t edge_cap = m_patch_info.edges_capacity[0];
+    const uint16_t face_cap = m_patch_info.faces_capacity[0];
+
+    // copy ev to shared memory
+    m_s_ev = shrd_alloc.alloc<uint16_t>(2 * edge_cap);
+    detail::load_async(block,
+                       reinterpret_cast<uint16_t*>(m_patch_info.ev),
+                       2 * m_s_num_edges[0],
+                       m_s_ev,
+                       false);
+
+    // alloc shared memory for all vertex attributes
+    m_s_vwgt     = shrd_alloc.alloc<uint16_t>(vert_cap);
+    m_s_vdegree  = shrd_alloc.alloc<uint16_t>(vert_cap);
+    m_s_vadjewgt = shrd_alloc.alloc<uint16_t>(vert_cap);
+
+    // alloc shared memory for edge attrobutes
+    m_s_ewgt = shrd_alloc.alloc<uint16_t>(edge_cap);
+
+    // alloc shared memory for matching result
+    m_s_matching = shrd_alloc.alloc<uint16_t>(vert_cap);
+    fill_n<blockThreads>(m_s_matching, vert_cap, uint16_t(INVALID16));
+
+    // alloc shared memory for mapping array
+    m_s_mapping = shrd_alloc.alloc<uint16_t>(2 * edge_cap);
+    m_s_unmapping = shrd_alloc.alloc<uint16_t>(2 * edge_cap);
+
+    block.sync();
+}
 
 }  // namespace rxmesh
--- apps/NDReorder/nd_reorder_kernel.cuh
+++ apps/NDReorder/nd_reorder_kernel.cuh
@@ -8,19 +8,53 @@
 #include "rxmesh/cavity_manager.cuh"
 #include "rxmesh/rxmesh_dynamic.h"
 
+// using void has type error?
 template <typename T, uint32_t blockThreads>
-__global__ static void nd_main(const rxmesh::Context context)
+__device__ __inline__ int random_matching(rxmesh::Context& context, uint16_t* matching_arr) {
+
+
+    using namespace rxmesh;
+    
+    auto rm_lambda = [&](VertexHandle v_id, VertexIterator& vi){
+        VertexHandle match_v = vi[0];
+
+        uint16_t v_local_id = v_id.local_id();
+        uint16_t match_v_local_id = match_v.local_id();
+
+        atomicCAS(&matching_arr[match_v_local_id], INVALID16, v_local_id);
+        // TODO: matching in progress
+
+        // 1. two hop implementation
+        // 2. admed implementation
+
+    };
+    
+    auto block = cooperative_groups::this_thread_block();
+
+    Query<blockThreads> query(context);
+    query.dispatch<Op::VV>(block, shrd_alloc, rm_lambda);
+
+    query.dispatch<Op::VE>(block, shrd_alloc, ve_lambda);
+
+    return 0;
+}
+
+
+template <typename T, uint32_t blockThreads>
+__global__ static void nd_main(rxmesh::Context context)
 {
     using namespace rxmesh;
     auto           block = cooperative_groups::this_thread_block();
     ShmemAllocator shrd_alloc;
     CoarsePatchinfo<blockThreads> coarsen_graph(block, context, shrd_alloc);
 
+    // TODO: coarsen and do the multi-level partition and then refinement
+
     // iteration num known before kernel -> shared mem known before kernel
     int i = 0;
     while (i < 1) {
         // device matching query specifically on CoarsePatchinfo
-        // heavy_edge_matching(coarsen_graph, matching_array);
+        random_matching(coarsen_graph, matching_array, shrd_alloc);
 
         // // coarsen graph
         // graph_coarsening(coarsen_graph, matching_array);
