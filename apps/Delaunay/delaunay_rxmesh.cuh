5cdca4f8dd14c613a6bb5b4ebc3ec078ffa94df4
ahmed <ahmahmoud@ucdavis.edu>
2024-04-12 19:12:14 -0400

    blah

--- apps/Delaunay/delaunay_rxmesh.cuh
+++ apps/Delaunay/delaunay_rxmesh.cuh
@@ -301,12 +301,6 @@ inline void delaunay_rxmesh(rxmesh::RXMeshDynamic& rx, bool with_verify = true)
         rx.reset_scheduler();
         int inner_iter = 0;
         while (!rx.is_queue_empty()) {
-            RXMESH_INFO("outer_iter= {}, inner_iter = {}, queue size= {}",
-                        outer_iter,
-                        inner_iter++,
-                        rx.get_context().m_patch_scheduler.size());
-
-
             LaunchBox<blockThreads> launch_box;
             rx.prepare_launch_box(
                 {Op::EVDiamond, Op::VV},
