5451b9119062ce861af3168f974054b66828bcd8
ahdhn <ahmahmoud@ucdavis.edu>
2024-05-08 23:01:13 -0400

    minor fix

--- apps/ShortestEdgeCollapse/benchmark.sh
+++ apps/ShortestEdgeCollapse/benchmark.sh
@@ -15,7 +15,7 @@ num_run=1
 device_id=1
 
 for file in $input_dir/*.obj; do 	 
-    if [ -f "$file" ]&& [ "$(basename "$file")" != "Engine_Assy.obj" ] && [ "$(basename "$file")" != "Chain_Hoist.obj" ] && [ "$(basename "$file")" != "Beethoven.obj" ] && [ "$(basename "$file")" != "Upper_Capsule.obj" ] && [ "$(basename "$file")" != "Jellyfish_lamp_part_A__B.obj" ]; then
+    if [ -f "$file" ]; then
 		echo $exe -input "$file" -num_iter 3 -device_id $device_id
         $exe -input "$file" -num_iter 3 -device_id $device_id
     fi 
