5bf05053db602cc6f832fe77246ab02bdae9736c
ahdhn <ahmahmoud@ucdavis.edu>
2024-05-08 23:00:33 -0400

    minor fix

--- apps/ShortestEdgeCollapse/benchmark.sh
+++ apps/ShortestEdgeCollapse/benchmark.sh
@@ -2,6 +2,7 @@
 #echo "Please make sure to first compile the source code and then enter the input OBJ files directory."
 #read -p "OBJ files directory (no trailing slash): " input_dir
 
+input_dir="/home/ahmed/RXMesh/input"
 echo "Input directory= $input_dir"
 exe="../../build/bin/ShortestEdgeCollapse"
 
@@ -14,7 +15,7 @@ num_run=1
 device_id=1
 
 for file in $input_dir/*.obj; do 	 
-    if [ -f "$file" ]; then
+    if [ -f "$file" ]&& [ "$(basename "$file")" != "Engine_Assy.obj" ] && [ "$(basename "$file")" != "Chain_Hoist.obj" ] && [ "$(basename "$file")" != "Beethoven.obj" ] && [ "$(basename "$file")" != "Upper_Capsule.obj" ] && [ "$(basename "$file")" != "Jellyfish_lamp_part_A__B.obj" ]; then
 		echo $exe -input "$file" -num_iter 3 -device_id $device_id
         $exe -input "$file" -num_iter 3 -device_id $device_id
     fi 
