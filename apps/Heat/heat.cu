bbafbd90d9b79628b19b8690345c0f3fc6eed88f
ahmed <ahmahmoud@ucdavis.edu>
2024-07-19 11:15:40 -0400

    apps templates

---
+++ apps/Heat/heat.cu
@@ -0,0 +1,21 @@
+#include "rxmesh/query.cuh"
+#include "rxmesh/rxmesh_static.h"
+
+#include "rxmesh/matrix/sparse_matrix.cuh"
+
+using namespace rxmesh;
+
+
+int main(int argc, char** argv)
+{
+    Log::init();
+
+    const uint32_t device_id = 0;
+    cuda_query(device_id);
+
+    RXMeshStatic rx(STRINGIFY(INPUT_DIR) "sphere3.obj");
+
+#if USE_POLYSCOPE
+    polyscope::show();
+#endif
+}
