55171332f8444f86038ac053fd8421197e7e9e12
Sachin Kishan <sachinkishan02@gmail.com>
2024-08-02 23:48:17 +0530

    working matrix set up

--- apps/SCP/scp.cu
+++ apps/SCP/scp.cu
@@ -31,7 +31,7 @@ __device__ __forceinline__ T
 edge_cotan_weight(const rxmesh::VertexHandle&       p_id,
                   const rxmesh::VertexHandle&       r_id,
                   const rxmesh::VertexHandle&       q_id,
-                  const rxmesh::VertexHandle&       s_id,
+                  //const rxmesh::VertexHandle&       s_id,
                   const rxmesh::VertexAttribute<T>& X)
 {
     // Get the edge weight between the two vertices p-r where
@@ -40,18 +40,19 @@ edge_cotan_weight(const rxmesh::VertexHandle&       p_id,
     const vec3<T> p(X(p_id, 0), X(p_id, 1), X(p_id, 2));
     const vec3<T> r(X(r_id, 0), X(r_id, 1), X(r_id, 2));
     const vec3<T> q(X(q_id, 0), X(q_id, 1), X(q_id, 2));
-    const vec3<T> s(X(s_id, 0), X(s_id, 1), X(s_id, 2));
+    //const vec3<T> s(X(s_id, 0), X(s_id, 1), X(s_id, 2));
 
     // cotans[(v1, v2)] =np.dot(e1, e2) / np.linalg.norm(np.cross(e1, e2))
 
     float weight = 0;
     if (q_id.is_valid())
         weight += dot((p - q), (r - q)) / length(cross(p - q, r - q));
-    if (s_id.is_valid())
-        weight += dot((p - s), (r - s)) / length(cross(p - s, r - s));
-    weight /= 2;
+    
+    //weight /= 2;
     return std::max(0.f, weight);
 }
+
+
 
 
 template <typename T, uint32_t blockThreads>
@@ -63,9 +64,16 @@ __global__ static void compute_edge_weights_evd(
 
     auto vn_lambda = [&](EdgeHandle edge_id, VertexIterator& vv) {
         T e_weight = 0;
-        e_weight   = edge_cotan_weight(vv[0], vv[2], vv[1], vv[3], coords);
+
+        if (vv[1].is_valid())
+            e_weight += edge_cotan_weight(vv[0], vv[2], vv[1], coords);
+        if (vv[3].is_valid())
+            e_weight += edge_cotan_weight(vv[0], vv[2], vv[3], coords);
+        //if (vv[1].is_valid() && vv[3].is_valid())
+        e_weight /= 4;
         A_mat(vv[0], vv[2]) = e_weight;
         A_mat(vv[2], vv[0]) = e_weight;
+
     };
 
     auto                block = cooperative_groups::this_thread_block();
@@ -80,7 +88,6 @@ __global__ static void calculate_Ld_matrix(
     rxmesh::SparseMatrix<float> weight_mat,  // [num_coord, num_coord]
     rxmesh::SparseMatrix<cuComplex> Ld           // [num_coord, num_coord]
 )
-
 {
     auto init_lambda = [&](VertexHandle v_id, VertexIterator& vv) {
         Ld(v_id, v_id) = make_cuComplex(0, 0);
@@ -89,8 +96,10 @@ __global__ static void calculate_Ld_matrix(
             Ld(v_id, vv[nei_index]) = make_cuComplex(0, 0);
 
         for (int nei_index = 0; nei_index < vv.size(); nei_index++) {
-            //Ld(v_id, v_id) += make_cuComplex(weight_mat(v_id, vv[nei_index]),
-              //                             weight_mat(v_id, vv[nei_index]));
+            Ld(v_id, v_id) =
+                cuCaddf(Ld(v_id, v_id),
+                        make_cuComplex(weight_mat(v_id, vv[nei_index]),
+                                       weight_mat(v_id, vv[nei_index])));
 
             Ld(v_id, vv[nei_index]) =
                 cuCsubf(Ld(v_id, vv[nei_index]),make_cuComplex(weight_mat(v_id, vv[nei_index]), 0));
@@ -101,6 +110,26 @@ __global__ static void calculate_Ld_matrix(
     Query<blockThreads> query(context);
     ShmemAllocator      shrd_alloc;
     query.dispatch<Op::VV>(block, shrd_alloc, init_lambda);
+}
+
+template <typename T, uint32_t blockThreads>
+__global__ static void subtract_matrix(const rxmesh::Context   context,
+                                       rxmesh::SparseMatrix<T> A_mat,
+                                       rxmesh::SparseMatrix<T> B_mat,
+                                       rxmesh::SparseMatrix<T> C_mat)
+{
+
+    auto subtract = [&](VertexHandle v_id, VertexIterator& vv) {
+        for (int i = 0; i < vv.size(); ++i) {
+            A_mat(v_id, vv[i]) = 
+                cuCsubf(B_mat(v_id, vv[i]), C_mat(v_id, vv[i]));
+        }
+    };
+
+    auto                block = cooperative_groups::this_thread_block();
+    Query<blockThreads> query(context);
+    ShmemAllocator      shrd_alloc;
+    query.dispatch<Op::VV>(block, shrd_alloc, subtract);
 }
 
 int main(int argc, char** argv)
@@ -174,7 +203,17 @@ int main(int argc, char** argv)
             rx.get_context(), weight_matrix, Ld);
 
 
-            
+     SparseMatrix<cuComplex> Lc(rx);
+    rxmesh::LaunchBox<CUDABlockSize> launch_box_lc;
+    rx.prepare_launch_box({rxmesh::Op::VV},
+                          launch_box_ld,
+                          (void*)subtract_matrix<cuComplex, CUDABlockSize>);
+
+    subtract_matrix<cuComplex, CUDABlockSize>
+        <<<launch_box_ld.blocks,
+           launch_box_ld.num_threads,
+           launch_box_ld.smem_bytes_dyn>>>
+    (rx.get_context(), Lc, Ld, A);
 
 
 
