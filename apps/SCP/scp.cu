0dee66cc87d5f2aea3d13b5a125be1eb25333af0
Sachin Kishan <sachinkishan02@gmail.com>
2024-08-02 23:55:25 +0530

    successfully compiling combined code

--- apps/SCP/scp.cu
+++ apps/SCP/scp.cu
@@ -103,6 +103,8 @@ __global__ static void calculate_Ld_matrix(
 
             Ld(v_id, vv[nei_index]) =
                 cuCsubf(Ld(v_id, vv[nei_index]),make_cuComplex(weight_mat(v_id, vv[nei_index]), 0));
+
+
         }
     };
 
@@ -216,13 +218,70 @@ int main(int argc, char** argv)
            launch_box_ld.smem_bytes_dyn>>>
     (rx.get_context(), Lc, Ld, A);
 
+    int number_of_vertices = rx.get_num_vertices();
 
 
-    //vertex_normals->move(rxmesh::DEVICE, rxmesh::HOST);
+    DenseMatrix<cuComplex> eb(rx, number_of_vertices, 1);
+    DenseMatrix<cuComplex> u(rx, number_of_vertices, 1);
+    DenseMatrix<cuComplex> T1(rx, number_of_vertices, 1);
+
+    DenseMatrix<cuComplex> y(rx, number_of_vertices, 1);
 
 
+    SparseMatrix<cuComplex> B(rx);
 
-    //Lc
+    uint32_t num_bd_vertices = 0;
+
+    rx.for_each_vertex(
+        HOST,
+        [&](const VertexHandle& vh) {
+            if (boundaryVertices(vh)) {
+                num_bd_vertices++;
+            }
+        },
+        NULL,
+        false);
+
+
+    rx.for_each_vertex(rxmesh::DEVICE,
+                       [B, eb, boundaryVertices, num_bd_vertices] __device__(
+                           const rxmesh::VertexHandle vh) mutable {
+                           eb(vh, 0) = make_cuComplex(
+                               boundaryVertices(vh, 0) / num_bd_vertices, 0.0f);
+                           B(vh, vh) =
+                               make_cuComplex(boundaryVertices(vh, 0), 0.0f);
+                       });
+
+    B.move(rxmesh::DEVICE, rxmesh::HOST);
+    eb.move(rxmesh::DEVICE, rxmesh::HOST);
+
+    //
+    // S = [B- (1/Vb) * ebebT];
+
+    cuComplex T2 = eb.dot(u);
+
+    B.multiply(u, T1);
+
+    eb.multiply(T2);
+
+    rx.for_each_vertex(
+        rxmesh::DEVICE,
+        [eb, B, T2, T1] __device__(const rxmesh::VertexHandle vh) mutable {
+            T1(vh, 0) = cuCsubf(T1(vh, 0), eb(vh, 0));
+        });
+
+    Lc.pre_solve(PermuteMethod::NSTDIS);  // can be outside the loop
+    Lc.solve(T1, y);                      // Ly=T1
+
+    y.multiply(1 / y.norm2());
+    u.copy_from(y);
+
+    // conversion step
+
+    rx.get_polyscope_mesh()->addVertexScalarQuantity("vBoundary",
+                                                     boundaryVertices);
+
+
 
 
 #if USE_POLYSCOPE
