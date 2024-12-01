c65e4a3fddbd75bd598d662a9f4fa4f0cf665525
Sachin Kishan <sachinkishan02@gmail.com>
2024-12-01 17:54:18 +0530

    sampler function

--- apps/GravoMG/gmg.cu
+++ apps/GravoMG/gmg.cu
@@ -17,6 +17,34 @@ using namespace rxmesh;
  *
  */
 
+template <typename T, uint32_t blockThreads>
+__global__ static void sample_points(
+    const rxmesh::Context      context,
+    rxmesh::VertexAttribute<T> vertex_pos,
+    rxmesh::VertexAttribute<T> distance,
+    rxmesh::SparseMatrix<T>    weight_matrix)
+{
+    auto sampler = [&](VertexHandle v_id, VertexIterator& vv)
+    {
+        for (int i = 0; i < vv.size(); i++) 
+        {
+            float dist = sqrt(pow(vertex_pos(v_id, 0) - vertex_pos(v_id, 0), 2) +
+                              pow(vertex_pos(v_id, 1) - vertex_pos(v_id, 1), 2) +
+                              pow(vertex_pos(v_id, 2) - vertex_pos(v_id, 2), 2))+ distance(vv[i], 0);
+            if (dist < distance(v_id,0)) 
+            {
+                distance(v_id, 0) = dist;
+            }
+        }
+    };
+    auto                block = cooperative_groups::this_thread_block();
+    Query<blockThreads> query(context);
+    ShmemAllocator      shrd_alloc;
+    bool                done = false;
+    query.dispatch<Op::VV>(block, shrd_alloc,sampler);
+}
+
+
 int main(int argc, char** argv)
 {
     Log::init();
@@ -30,6 +58,7 @@ int main(int argc, char** argv)
 
     //attribute to sample,store and order samples
     auto samples = *rx.add_vertex_attribute<float>("samples", 1);
+    auto distance = *rx.add_vertex_attribute<float>("distance", 1);
 
 
 
@@ -52,23 +81,28 @@ int main(int argc, char** argv)
     auto context = rx.get_context();
     rx.for_each_vertex(
         rxmesh::DEVICE,
-        [vertex_pos, seed,context,samples] __device__(const rxmesh::VertexHandle vh) {
+        [vertex_pos, seed,context,samples,distance] __device__(const rxmesh::VertexHandle vh) {
 
         if (seed == context.linear_id(vh)) {
-                               samples(vh, 0) = 10;
+            samples(vh, 0) = 10;
+            distance(vh, 0) = 0;
+
             printf("\nFOUND");
         }
-                           else
-                               samples(vh, 0) = 0;
+        else {
+            samples(vh, 0) = 0;
+            distance(vh,0)=INFINITY;
+        }
         printf("\nLocal: %d Patch: %d Unique: %u",
-                     vh.local_id(),
-                     vh.patch_id(),
-                     context.linear_id(vh));
-            
+            vh.local_id(),
+            vh.patch_id(),
+            context.linear_id(vh));
         });
 
     samples.move(DEVICE, HOST);
+    distance.move(DEVICE, HOST);
     rx.get_polyscope_mesh()->addVertexScalarQuantity("samples", samples);
+    rx.get_polyscope_mesh()->addVertexScalarQuantity("distance", distance);
 
 
 
