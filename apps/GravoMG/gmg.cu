af458e2f0e9d0790c4aa80e5514fc1b390301dfb
Sachin Kishan <sachinkishan02@gmail.com>
2024-12-19 17:15:28 +0530

    fully sampled mesh

--- apps/GravoMG/gmg.cu
+++ apps/GravoMG/gmg.cu
@@ -1,4 +1,5 @@
 #include "rxmesh/query.cuh"
+#include "rxmesh/reduce_handle.h"
 #include "rxmesh/rxmesh_static.h"
 
 #include "rxmesh/matrix/sparse_matrix.cuh"
@@ -6,43 +7,38 @@
 using namespace rxmesh;
 
 
-//farthest point sampling
-
-/*
- *
- * pick a random point on the mesh
- * sample the furthest point, add it to a list
- * do a foreach vertex operation to determine furthest
- * make sure the list u get finally has some ordering
- *
- */
-
 template <typename T, uint32_t blockThreads>
-__global__ static void sample_points(
-    const rxmesh::Context      context,
-    rxmesh::VertexAttribute<T> vertex_pos,
-    rxmesh::VertexAttribute<T> distance,
-    rxmesh::SparseMatrix<T>    weight_matrix)
+__global__ static void sample_points(const rxmesh::Context      context,
+                                     rxmesh::VertexAttribute<T> vertex_pos,
+                                     rxmesh::VertexAttribute<T> distance,
+                                     int*                       flag)
 {
-    auto sampler = [&](VertexHandle v_id, VertexIterator& vv)
-    {
-        for (int i = 0; i < vv.size(); i++) 
-        {
-            float dist = sqrt(pow(vertex_pos(v_id, 0) - vertex_pos(v_id, 0), 2) +
-                              pow(vertex_pos(v_id, 1) - vertex_pos(v_id, 1), 2) +
-                              pow(vertex_pos(v_id, 2) - vertex_pos(v_id, 2), 2))+ distance(vv[i], 0);
-            if (dist < distance(v_id,0)) 
-            {
+
+    auto sampler = [&](VertexHandle v_id, VertexIterator& vv) {
+        for (int i = 0; i < vv.size(); i++) {
+
+            float dist =
+                sqrtf(powf(vertex_pos(v_id, 0) - vertex_pos(vv[i], 0), 2) +
+                      powf(vertex_pos(v_id, 1) - vertex_pos(vv[i], 1), 2) +
+                      powf(vertex_pos(v_id, 2) - vertex_pos(vv[i], 2), 2))+
+                distance(vv[i], 0);
+
+            //printf("\nVertex: %u Distance : %f", context.linear_id(v_id), dist);
+
+
+            if (dist < distance(v_id, 0)) {
                 distance(v_id, 0) = dist;
+                *flag             = 15;
             }
         }
+         //printf("\nFLAG : %d", *flag);
     };
     auto                block = cooperative_groups::this_thread_block();
     Query<blockThreads> query(context);
     ShmemAllocator      shrd_alloc;
-    bool                done = false;
-    query.dispatch<Op::VV>(block, shrd_alloc,sampler);
+    query.dispatch<Op::VV>(block, shrd_alloc, sampler);
 }
+
 
 
 int main(int argc, char** argv)
@@ -57,51 +53,99 @@ int main(int argc, char** argv)
     auto vertex_pos = *rx.get_input_vertex_coordinates();
 
     //attribute to sample,store and order samples
-    auto samples = *rx.add_vertex_attribute<float>("samples", 1);
+    auto sample_number = *rx.add_vertex_attribute<int>("sample_number", 1);
     auto distance = *rx.add_vertex_attribute<float>("distance", 1);
 
+    int* flagger;
+    cudaMallocManaged(&flagger, sizeof(int));
+    *flagger = 0;
+
+    auto context = rx.get_context();
+
+    constexpr uint32_t CUDABlockSize = 256;
+    rxmesh::LaunchBox<CUDABlockSize> lb;
+    rx.prepare_launch_box({rxmesh::Op::EVDiamond},
+                          lb,
+                          (void*)sample_points<float, CUDABlockSize>);
 
 
+
+    float ratio = 8;
+    int   numberOfLevels = 1;
+    int   numberOfSamples = 300;
+
+    
     int N = rx.get_num_vertices();
 
-    std::vector<int> sampleID(N);
+    std::random_device
+        rd;  // Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(
+        rd());  // Standard mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> dist(
+        0, N - 1);  // From 0 to (number of points - 1)
+    int seed = dist(gen);
 
-    Eigen::VectorXd D(N);
-    D.setConstant(std::numeric_limits<double>::infinity());
+    std::cout << "\nSeed: " << seed;
 
-    std::random_device rd;  // Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd());  // Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> dist(0, N - 1);  // From 0 to (number of points - 1)
-    const int seed = dist(gen);
+    VertexReduceHandle<float> reducer(distance);
+    cub::KeyValuePair<VertexHandle, float> farthestPoint;
 
-    sampleID[0] = seed;
-    std::cout << seed<<std::endl;
+    int j = 0;
+    for (int i = 0; i < numberOfSamples; i++) {
 
-//      vertex_pos.to_eigen<N>();
-    auto context = rx.get_context();
-    rx.for_each_vertex(
-        rxmesh::DEVICE,
-        [vertex_pos, seed,context,samples,distance] __device__(const rxmesh::VertexHandle vh) {
+        rx.for_each_vertex(rxmesh::DEVICE,
+                           [seed, context, sample_number, distance, i] __device__(
+                               const rxmesh::VertexHandle vh) {
+                               if (seed == context.linear_id(vh)) {
+                                   sample_number(vh, 0) = i;
+                                   distance(vh, 0)      = 0;
+                               }
+                               else {
+                                   if (i==0)
+                                   {
+                                       distance(vh, 0) = INFINITY;
+                                       sample_number(vh, 0) = -1;
 
-        if (seed == context.linear_id(vh)) {
-            samples(vh, 0) = 10;
-            distance(vh, 0) = 0;
+                                   }
+                               }
 
-            printf("\nFOUND");
-        }
-        else {
-            samples(vh, 0) = 0;
-            distance(vh,0)=INFINITY;
-        }
-        printf("\nLocal: %d Patch: %d Unique: %u",
-            vh.local_id(),
-            vh.patch_id(),
-            context.linear_id(vh));
-        });
+                           });
 
-    samples.move(DEVICE, HOST);
+        j = 0;
+        // we may need to call this repeatedly
+        do {
+            cudaDeviceSynchronize();
+            *flagger = 0;
+            sample_points<float, CUDABlockSize>
+                <<<lb.blocks, lb.num_threads, lb.smem_bytes_dyn>>>(
+                    rx.get_context(), vertex_pos, distance, flagger);
+            cudaDeviceSynchronize();
+            //std::cout << "\nflag: "<<*flagger
+              //        << "\n\niteration: " << j << std::endl;
+
+            j++;
+
+        } while (*flagger != 0);
+
+        // reduction step
+        farthestPoint = reducer.arg_max(distance, 0);
+        seed          = rx.linear_id(farthestPoint.key);
+
+    }
+
+
+    sample_number.move(DEVICE, HOST);
     distance.move(DEVICE, HOST);
-    rx.get_polyscope_mesh()->addVertexScalarQuantity("samples", samples);
+
+
+
+   
+    std::cout << std::endl << farthestPoint.value;
+
+    std::cout << std::endl << rx.linear_id(farthestPoint.key);
+
+    
+    rx.get_polyscope_mesh()->addVertexScalarQuantity("sample_number",sample_number);
     rx.get_polyscope_mesh()->addVertexScalarQuantity("distance", distance);
 
 
