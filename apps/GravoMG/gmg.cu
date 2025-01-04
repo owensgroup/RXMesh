546ab2c10662ced46f6a6035654eb66244c7fe18
Sachin Kishan <sachinkishan02@gmail.com>
2025-01-04 18:30:58 +0530

    first level operator construction

--- apps/GravoMG/gmg.cu
+++ apps/GravoMG/gmg.cu
@@ -7,6 +7,74 @@
 
 #include "cuda_runtime.h"
 
+// Function to compute the projected distance from a point to a triangle
+__device__ float projectedDistance(const Eigen::Vector3f& v0,
+                                   const Eigen::Vector3f& v1,
+                                   const Eigen::Vector3f& v2,
+                                   const Eigen::Vector3f& p)
+{
+    // Compute edges of the triangle
+    Eigen::Vector3f edge1 = v1 - v0;
+    Eigen::Vector3f edge2 = v2 - v0;
+
+    // Compute the triangle normal
+    Eigen::Vector3f normal        = edge1.cross(edge2);
+    float           normal_length = normal.norm();
+
+    if (normal_length < 1e-6f) {
+        return -1.0f;  // Return -1 to indicate an error
+    }
+
+    // Normalize the normal
+    normal.normalize();
+
+    // Compute vector from point to the triangle vertex
+    Eigen::Vector3f point_to_vertex = p - v0;
+
+    // Project the vector onto the normal
+    float distance = point_to_vertex.dot(normal);
+
+    // Return the absolute distance
+    return std::fabs(distance);
+}
+
+
+
+__device__ std::tuple<float, float, float> computeBarycentricCoordinates(
+    const Eigen::Vector3f& v0,
+    const Eigen::Vector3f& v1,
+    const Eigen::Vector3f& v2,
+    const Eigen::Vector3f& p)
+{
+    // Compute edges of the triangle
+    Eigen::Vector3f edge1    = v1 - v0;
+    Eigen::Vector3f edge2    = v2 - v0;
+    Eigen::Vector3f pointVec = p - v0;
+
+    // Compute normal of the triangle
+    Eigen::Vector3f normal = edge1.cross(edge2);
+    float area2 = normal.squaredNorm();  // Area of the triangle multiplied by 2
+
+    // Compute barycentric coordinates
+    float lambda0, lambda1, lambda2;
+
+    // Sub-area with respect to v0
+    Eigen::Vector3f normal1 = (v1 - p).cross(v2 - p);
+    lambda0                 = normal1.squaredNorm() / area2;
+
+    // Sub-area with respect to v1
+    Eigen::Vector3f normal2 = (v2 - p).cross(v0 - p);
+    lambda1                 = normal2.squaredNorm() / area2;
+
+    // Sub-area with respect to v2
+    Eigen::Vector3f normal3 = (v0 - p).cross(v1 - p);
+    lambda2                 = normal3.squaredNorm() / area2;
+
+    // Return the barycentric coordinates
+    return std::make_tuple(lambda0, lambda1, lambda2);
+}
+
+
 using namespace rxmesh;
 class BitMatrix
 {
@@ -19,15 +87,31 @@ class BitMatrix
     __host__ BitMatrix(int size) : N(size)
     {
         row_size = (N + 31) / 32;  // Number of integers per row
-        cudaMalloc(&d_data, row_size * N * sizeof(uint32_t));
-        cudaMemset(
-            d_data, 0, row_size * N * sizeof(uint32_t));  // Initialize to 0
+        cudaError_t err;
+
+        // Allocate memory
+        err = cudaMalloc(&d_data, row_size * N * sizeof(uint32_t));
+        if (err != cudaSuccess) {
+            throw std::runtime_error("cudaMalloc failed: " +
+                                     std::string(cudaGetErrorString(err)));
+        }
+
+        // Initialize to 0
+        err = cudaMemset(d_data, 0, row_size * N * sizeof(uint32_t));
+        if (err != cudaSuccess) {
+            cudaFree(d_data);  // Free allocated memory in case of failure
+            throw std::runtime_error("cudaMemset failed: " +
+                                     std::string(cudaGetErrorString(err)));
+        }
     }
 
     // Destructor
     __host__ ~BitMatrix()
     {
-        cudaFree(d_data);
+        if (d_data) {
+            cudaFree(d_data);
+            d_data = nullptr;
+        }
     }
 
     // Helper function to calculate index and bit position
@@ -46,6 +130,7 @@ class BitMatrix
         uint32_t  mask                 = 1 << bit_position;
 
         uint32_t old_val, new_val;
+        
         do {
             old_val = *address;  // Read the current value
             if (value) {
@@ -54,6 +139,7 @@ class BitMatrix
                 new_val = old_val & ~mask;  // Clear the bit
             }
         } while (atomicCAS(address, old_val, new_val) != old_val);
+        
     }
     __device__ inline bool trySet(int row, int col)
     {
@@ -62,13 +148,15 @@ class BitMatrix
         uint32_t  mask                 = 1 << bit_position;
 
         uint32_t old_val, new_val;
+        
         do {
             old_val = *address;  // Read the current value
             if (old_val & mask)
                 return false;          // Already set, another thread got it
             new_val = old_val | mask;  // Set the bit
-        } while (atomicCAS(address, old_val, new_val) != old_val);
-
+        }
+        while (atomicCAS(address, old_val, new_val) != old_val);
+        
         return true;  // Successfully set
     }
 
@@ -79,6 +167,8 @@ class BitMatrix
         return (value >> bit_position) & 1;
     }
 };
+
+
 
 
 template <typename T, uint32_t blockThreads>
@@ -94,16 +184,20 @@ __global__ static void findNumberOfCoarseNeighbors(
             
             if (clustered_vertices(v_id, 0) != clustered_vertices(vv[i], 0)) 
             {
-                if (bitMatrix.trySet(clustered_vertices(v_id, 0),
-                                     clustered_vertices(vv[i], 0))) {
-                    /* printf("\nFound %d with %d since the matrix value is 0",
-                           clustered_vertices(v_id, 0),
-                           clustered_vertices(vv[i], 0));
-                    */
+                int a = clustered_vertices(vv[i], 0);
+                int b = clustered_vertices(v_id, 0);
+
+                //something wrong here
+                if (bitMatrix.trySet(b,a)) {
+
+                    //printf("\nFound %d with %d since the matrix value is 0",
+                    //       clustered_vertices(v_id, 0),
+                    //       clustered_vertices(vv[i], 0));
+
                     atomicAdd(&number_of_neighbors[clustered_vertices(v_id, 0)],
                               1);
                 }
-
+                
 
             }
         }
@@ -183,6 +277,10 @@ __global__ static void sample_points(const rxmesh::Context      context,
 
 
 
+struct Vec3
+{
+    float x, y, z;
+};
 
 int main(int argc, char** argv)
 {
@@ -212,7 +310,7 @@ int main(int argc, char** argv)
 
     auto context = rx.get_context();
 
-    constexpr uint32_t CUDABlockSize = 256;
+    constexpr uint32_t CUDABlockSize = 512;
     rxmesh::LaunchBox<CUDABlockSize> lb;
     rx.prepare_launch_box({rxmesh::Op::VV},
                           lb,
@@ -223,7 +321,7 @@ int main(int argc, char** argv)
     float ratio = 8;
     int   N     = rx.get_num_vertices();
     int   numberOfLevels = 1;
-    int   currentLevel    = 1; //first coarse mesh
+    int   currentLevel    = 2; //first coarse mesh
     int   numberOfSamples = N / powf(ratio , currentLevel);
     
 
@@ -236,6 +334,11 @@ int main(int argc, char** argv)
 
     VertexReduceHandle<float> reducer(distance);
     cub::KeyValuePair<VertexHandle, float> farthestPoint;
+
+    Vec3* vertices;
+
+    // Allocate unified memory
+    cudaMallocManaged(&vertices, numberOfSamples * sizeof(Vec3));
 
 
     // pre processing step
@@ -250,12 +353,16 @@ int main(int argc, char** argv)
                             sample_level_bitmask,
                             distance,
                             i, 
-                            currentLevel] __device__(
+                            currentLevel,
+                           vertex_pos,
+                           vertices] __device__(
                                const rxmesh::VertexHandle vh) {
                                if (seed == context.linear_id(vh)) {
                                    sample_number(vh, 0) = i;
                                    distance(vh, 0)      = 0;
-
+                                   vertices[i].x        = vertex_pos(vh, 0);
+                                   vertices[i].y        = vertex_pos(vh, 1);
+                                   vertices[i].z        = vertex_pos(vh, 2);
                                    /*
                                     *something like
                                     *
@@ -340,25 +447,31 @@ int main(int argc, char** argv)
     clustered_vertex.move(DEVICE, HOST);
     std::cout << "\Clustering iterations: " << j;
 
+
+    ////
+    ///
     //find number of neighbors x
     int* number_of_neighbors;
     cudaMallocManaged(&number_of_neighbors, numberOfSamples * sizeof(int));
     for (int i = 0; i < numberOfSamples; i++) {
         number_of_neighbors[i] = 0;
     }
-    BitMatrix bitMatrix(numberOfSamples * numberOfSamples);
+    BitMatrix bitMatrix(numberOfSamples);
     cudaDeviceSynchronize();
+
+    //ASSERT here that every vertex has a neighbor
 
     rxmesh::LaunchBox<CUDABlockSize> nn;
     rx.prepare_launch_box(
         {rxmesh::Op::VV}, nn, (void*)findNumberOfCoarseNeighbors<float, CUDABlockSize>);
 
 
+
     findNumberOfCoarseNeighbors<float, CUDABlockSize>
         <<<nn.blocks, nn.num_threads, nn.smem_bytes_dyn>>>(
             rx.get_context(), clustered_vertex, bitMatrix,number_of_neighbors);
     cudaDeviceSynchronize();
-
+    
     // construct row pointers -> prefix sum
     // Number of rows in your matrix
     int num_rows = numberOfSamples;  // Set this appropriately
@@ -372,7 +485,7 @@ int main(int argc, char** argv)
     // Temporary storage for CUB
     void*  d_cub_temp_storage = nullptr;
     size_t temp_storage_bytes = 0;
-
+    
     // Compute the required temporary storage size
     cub::DeviceScan::ExclusiveSum(d_cub_temp_storage,
                                   temp_storage_bytes,
@@ -380,20 +493,18 @@ int main(int argc, char** argv)
                                   row_ptr,
                                   num_rows + 1);
 
-    // Allocate temporary storage
     cudaMallocManaged(&d_cub_temp_storage, temp_storage_bytes);
-
-    // Perform the prefix sum
     cub::DeviceScan::ExclusiveSum(d_cub_temp_storage,
                                   temp_storage_bytes,
                                   number_of_neighbors,
                                   row_ptr,
                                   num_rows + 1);
 
-    cudaDeviceSynchronize();  // Synchronize to ensure prefix sum completion
-
-    // Free the temporary storage
+    cudaDeviceSynchronize();  
     cudaFree(d_cub_temp_storage);
+
+    //ASSERT the memory may not be enough, 
+
     /*
     printf("\nFINAL ROW POINTERS\n");
     for (int i = 0; i <= num_rows; ++i) {
@@ -405,17 +516,20 @@ int main(int argc, char** argv)
 
 
     // then populate the row pointers
+    
     int* value_ptr;
 
     cudaMallocManaged(&value_ptr, row_ptr[num_rows] * sizeof(int));
 
+
+    // make this based on thresh
     rx.for_each_vertex(
         rxmesh::DEVICE,
         [sample_number,
          bitMatrix,
          number_of_neighbors, row_ptr,
          value_ptr, numberOfSamples] __device__(const rxmesh::VertexHandle v_id) {
-        int number = sample_number(v_id, 0);
+        int number = sample_number(v_id, 0); 
         if (number != -1) {
             /// find start pointer
             int start_pointer = row_ptr[number];
@@ -432,6 +546,8 @@ int main(int argc, char** argv)
                     j++;
                 }
             }
+            //#ifndef NDEBUG
+            //THIS CAN BE AN ASSERT
             if (j != number_of_neighbors[number]) {
                 printf(
                     "ERROR: Number of neighbors does not match number of "
@@ -441,8 +557,9 @@ int main(int argc, char** argv)
     });
 
     cudaDeviceSynchronize();
+
     printf("\nCSR Array: \n");
-     for (int i = 0; i <= num_rows; ++i) {
+     for (int i = 0; i < num_rows; ++i) {
         printf("row_ptr[%d] = %d\n", i, row_ptr[i]);
         printf("add %d values\n", number_of_neighbors[i]);
         for (int q = row_ptr[i]; q < row_ptr[i + 1]; q++) {
@@ -461,6 +578,143 @@ int main(int argc, char** argv)
          context] __device__(const rxmesh::VertexHandle vh) {
                            number_of_neighbors_coarse(vh, 0) = number_of_neighbors[sample_number(vh, 0)];
         });
+
+        
+    //for each vertex, get a triangle
+    //project
+    /// for a fine vertex -> get source, get closest triangle, project
+    ///
+    ///
+
+    //csr
+    // row pointer
+    // value pointer
+    //
+    // projectToTriangle -> take a vertex in (row pointer index value), return the projected barycentric coordinates
+    // take those bary coords, put them into prolongation matrix -> normal sparse matrix for now
+    //
+
+    float* prolongation_operator;
+    cudaMallocManaged(&prolongation_operator, N * numberOfSamples*sizeof(float));
+
+    cudaDeviceSynchronize();
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < numberOfSamples; j++) {
+            prolongation_operator[numberOfSamples * i + j] = 0;
+        }
+    }
+
+    rx.for_each_vertex(
+        rxmesh::DEVICE,
+        [sample_number,
+         clustered_vertex,
+         number_of_neighbors,
+         row_ptr,value_ptr,
+         context,
+        vertices,vertex_pos, prolongation_operator,numberOfSamples] __device__(const rxmesh::VertexHandle vh) {
+
+        //go through every triangle of my cluster
+        const int cluster_point = clustered_vertex(vh, 0);
+        const int start_pointer = row_ptr[clustered_vertex(vh,0)];
+        const int end_pointer = row_ptr[clustered_vertex(vh,0)+1];
+
+        float min_distance = 99999;
+        Eigen::Vector3<float> selectedv1{0,0,0}, selectedv2{0, 0, 0},
+            selectedv3{0, 0, 0};
+        const Eigen::Vector3<float> q{
+            vertex_pos(vh, 0), vertex_pos(vh, 1), vertex_pos(vh, 2)};
+
+        int neighbor=0;
+        int selected_neighbor=0;
+        int neighbor_of_neighbor=0;
+        int selected_neighbor_of_neighbor=0;
+
+
+        for (int i=start_pointer;i<end_pointer;i++) {
+
+            float distance;
+             // Get the neighbor vertex
+            neighbor = value_ptr[i];  // Assuming col_idx stores column
+                                        // indices of neighbors in CSR.
+
+            // Get the range of neighbors for this neighbor
+            const int neighbor_start = row_ptr[neighbor];
+            const int neighbor_end   = row_ptr[neighbor + 1];
+
+            for (int j = neighbor_start; j < neighbor_end; j++) {
+                neighbor_of_neighbor = value_ptr[j];
+
+                for (int k=i+1;k<end_pointer;k++)
+                {
+                    if (value_ptr[k]==neighbor_of_neighbor) 
+                    {
+
+                        /* printf("\n%d %d and %d are a triangle",
+                               cluster_point,
+                               neighbor,
+                               neighbor_of_neighbor);
+                               */
+
+
+
+                        
+                        Eigen::Vector3<float> v1{vertices[cluster_point].x,
+                                                 vertices[cluster_point].y,
+                                                 vertices[cluster_point].z};
+                        Eigen::Vector3<float> v2{vertices[neighbor].x,
+                                                 vertices[neighbor].y,
+                                                 vertices[neighbor].z};
+                        Eigen::Vector3<float> v3{
+                            vertices[neighbor_of_neighbor].x,
+                            vertices[neighbor_of_neighbor].y,
+                            vertices[neighbor_of_neighbor].z};
+
+                        //find distance , if less than min dist, find bary coords, save them
+                        float distance = projectedDistance(v1, v2, v3, q);
+                        if (distance<min_distance) {
+                            
+                            min_distance = distance;
+                            selectedv1   = v1;
+                            selectedv2   = v2;
+                            selectedv3   = v3;
+                            selected_neighbor = neighbor;
+                            selected_neighbor_of_neighbor =neighbor_of_neighbor;
+                        }
+                    }
+                }
+            }
+        }
+        // take the best bary coords
+        auto [b1, b2, b3] = computeBarycentricCoordinates(
+            selectedv1, selectedv2, selectedv3, q);
+        // put it inside prolongation row, it will be unique so no race
+        // condition
+        int l = context.linear_id(vh);
+
+        printf("\n %d final coords: %f %f %f", l, b1, b2, b3);
+
+
+        prolongation_operator[l * numberOfSamples + cluster_point]        = b1;
+        prolongation_operator[l * numberOfSamples + selected_neighbor]             = b2;
+        prolongation_operator[l * numberOfSamples + selected_neighbor_of_neighbor] = b3;
+
+    });
+    cudaDeviceSynchronize();
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    
+    for (int i=0;i<N;i++) {
+        std::cout << std::endl << i << "  ";
+        for (int j=0;j<numberOfSamples;j++) {
+            std::cout << prolongation_operator[i * numberOfSamples + j] << " ";
+        }
+    }
+    
+
+
+
+
 
     number_of_neighbors_coarse.move(DEVICE, HOST);
 
