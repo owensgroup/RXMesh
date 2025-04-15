85bcb3c1b650a016dd735774fe8609f7e6544afb
ahmed <a.has.mahmoud@gmail.com>
2025-04-15 09:54:23 -0400

    fixes to cholesky and qr solve_hl_api api

--- include/rxmesh/matrix/cholesky_solver.h
+++ include/rxmesh/matrix/cholesky_solver.h
@@ -78,9 +78,29 @@ struct CholeskySolver : public DirectSolver<SpMatT, DenseMatOrder>
     {
         CUSOLVER_ERROR(cusolverSpSetStream(m_cusolver_sphandle, stream));
 
-        for (int i = 0; i < B_mat.cols(); ++i) {
-            cusolver_cholesky(B_mat.col_data(i, DEVICE),
-                              X_mat.col_data(i, DEVICE));
+        // the case where we solve for multiple rhs
+        if (m_mat->cols() == X_mat.rows() && m_mat->rows() == B_mat.rows() &&
+            X_mat.cols() == B_mat.cols()) {
+            for (int i = 0; i < B_mat.cols(); ++i) {
+                cusolver_cholesky(B_mat.col_data(i, DEVICE),
+                                  X_mat.col_data(i, DEVICE));
+            }
+        } else if (m_mat->cols() == X_mat.rows() * X_mat.cols() &&
+                   m_mat->rows() == B_mat.rows() * B_mat.cols()) {
+            // the case where we flatten X and B and do one solve
+            cusolver_cholesky(B_mat.col_data(0, DEVICE),
+                              X_mat.col_data(0, DEVICE));
+        } else {
+            RXMESH_ERROR(
+                "CholeskySolver::solve_hl_api() The sparse matrix size ({}, "
+                "{}) does not match with the rhs size ({}, {}) and the unknown "
+                "size ({}, {})",
+                m_mat->rows(),
+                m_mat->cols(),
+                B_mat.rows(),
+                B_mat.cols(),
+                X_mat.rows(),
+                X_mat.cols());
         }
     }
 
