11d7803b82e4f1564c65e0e31955a7af398e3708
Sachin Kishan <sachinkishan02@gmail.com>
2025-04-29 10:07:37 +0530

    remove unnecessary memory alloc from jacobi

--- include/rxmesh/matrix/gmg/jacobi_solver.h
+++ include/rxmesh/matrix/gmg/jacobi_solver.h
@@ -6,7 +6,6 @@
 #include "rxmesh/matrix/sparse_matrix.h"
 
 namespace rxmesh {
-
 template <typename T>
 struct JacobiSolver
 {
@@ -21,45 +20,34 @@ struct JacobiSolver
     {
     }
 
-
     template <int numCol>
     __host__ void solve(const SparseMatrix<T>& A,
-                        const DenseMatrix<T>&   b,
-                        DenseMatrix<T>&         x,
-                        int                     num_iter)
+                        const DenseMatrix<T>&  b,
+                        DenseMatrix<T>&        x,
+                        int                    num_iter)
     {
         constexpr uint32_t blockThreads = 256;
+        uint32_t           blocks       = DIVIDE_UP(A.rows(), blockThreads);
 
-        uint32_t blocks = DIVIDE_UP(A.rows(), blockThreads);
-
-        DenseMatrix<T> x_new = DenseMatrix<T>(A.rows(), x.cols());  // m_x_new;
-
-        // do this alloc before the runtime solving
-        m_x_new = DenseMatrix<T>(A.rows(), x.cols());
+        if (m_x_new.rows() != A.rows() || m_x_new.cols() != x.cols()) {
+            m_x_new = DenseMatrix<T>(A.rows(), x.cols());
+        }
 
         for (int iter = 0; iter < num_iter; ++iter) {
-
             for_each_item<<<blocks, blockThreads>>>(
-
-                A.rows(), [x_new, A, b, x] __device__(int row) mutable {
+                A.rows(),
+                [x_new = m_x_new, A, b, x] __device__(int row) mutable {
                     T    diag     = 0.0f;
                     bool has_diag = false;
-
-                    T sum[numCol];
-
+                    T    sum[numCol];
                     for (int c = 0; c < numCol; ++c) {
                         sum[c] = 0;
                     }
-
                     int start = A.row_ptr()[row];
                     int stop  = A.row_ptr()[row + 1];
-
                     for (int j = start; j < stop; ++j) {
-
                         int col = A.col_idx()[j];
-
-                        T val = A.get_val_at(j);
-
+                        T   val = A.get_val_at(j);
                         if (col == row) {
                             // If it's a diagonal element, store its value
                             diag     = val;
@@ -71,22 +59,18 @@ struct JacobiSolver
                             }
                         }
                     }
-
                     assert(has_diag);
-
                     if (has_diag && abs(diag) > 10e-8f) {
                         for (int c = 0; c < numCol; ++c) {
                             x_new(row, c) = (b(row, c) - sum[c]) / diag;
                         }
-
                     } else {
                         for (int c = 0; c < numCol; ++c) {
                             x_new(row, c) = x(row, c);
                         }
                     }
                 });
-
-            x.swap(x_new);
+            x.swap(m_x_new);
         }
     }
 };
