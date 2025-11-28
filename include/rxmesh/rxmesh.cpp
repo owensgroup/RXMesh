73c9d9fd932b5ddc44a0efe7f03c992a5baad207
Behrooz <bzareb01@gmail.com>
2025-11-27 19:15:39 -0500

    update

--- include/rxmesh/rxmesh.cpp
+++ include/rxmesh/rxmesh.cpp
@@ -298,7 +298,7 @@ void RXMesh::build(const std::vector<std::vector<uint32_t>>& fv,
                                                            m_edges_map,
                                                            m_num_vertices,
                                                            m_num_edges,
-                                                           patching_with_metis);
+                                                           false);
         } else {
             m_patcher = std::make_unique<patcher::Patcher>(patcher_file);
         }
@@ -310,7 +310,7 @@ void RXMesh::build(const std::vector<std::vector<uint32_t>>& fv,
                                                        m_edges_map,
                                                        m_num_vertices,
                                                        m_num_edges,
-                                                       patching_with_metis);
+                                                       false);
     }
 
 
