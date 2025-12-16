0362c5eaafe81b84c203f8b9552b87d010db5e40
ahmed <a.has.mahmoud@gmail.com>
2025-12-16 15:56:39 -0500

    gauss newton

--- include/rxmesh/diff/gauss_newton_solver.h
+++ include/rxmesh/diff/gauss_newton_solver.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <optional>
+
 #include "rxmesh/diff/diff_vector_problem.h"
 
 #include "rxmesh/matrix/cg_solver.h"
@@ -18,11 +20,13 @@ struct GaussNetwtonSolver
     using SpMatT       = SparseMatrix<T>;
     using DenseMatT    = typename DiffProblemT::DenseMatT;
     using SolverT      = cuDSSCholeskySolver<SpMatT, DenseMatT::OrderT>;
+    using IndexT       = typename SpMatT::IndexT;
 
-    DiffProblemT& problem;
-    DenseMatT     dir;
-    SolverT       solver;
-    SpMatT        JtJ;
+    DiffProblemT&          problem;
+    DenseMatT              dir;
+    std::optional<SolverT> solver;
+    SpMatT                 JtJ;
+    SpMatT                 jac_trans;
 
     bool is_prep_solver_called;
 
@@ -32,7 +36,10 @@ struct GaussNetwtonSolver
      * @brief Newton solver
      */
     GaussNetwtonSolver(DiffProblemT& p)
-        : problem(p), solve_time(0), is_prep_solver_called(false)
+        : problem(p),
+          solve_time(0),
+          is_prep_solver_called(false),
+          m_JtJ_nnz_expected(-1)
     {
         dir.reset(0, LOCATION_ALL);
     }
@@ -52,8 +59,8 @@ struct GaussNetwtonSolver
         create_JtJ();
 
         // create an instance of SolverT (solver)
-        solver = SolverT(&JtJ);
-        solver.pre_solve(problem.rx, *problem.grad, dir);
+        solver.emplace(&JtJ);
+        solver->pre_solve(problem.rx, *problem.grad, dir);
     }
 
     /**
@@ -66,28 +73,297 @@ struct GaussNetwtonSolver
             prep_solver();
         }
 
-        // TODO compute JtJ
-        create_JtJ();
+        // compute JtJ
+        compute_JtJ();
 
         // solve for new direction
 
         GPUTimer timer;
         timer.start();
 
-        solver.pre_solve(problem.rx, dir, *problem.grad);
-        solver.solve(*problem.grad, dir, stream);
+        solver->pre_solve(problem.rx, dir, *problem.grad);
+        solver->solve(*problem.grad, dir, stream);
 
 
         timer.stop();
         solve_time += timer.elapsed_millis();
     }
 
+    /**
+     * @brief release all memory and resources
+     */
+    __host__ void release()
+    {
+        // SpGEMM reuse resources
+        if (m_spgemmDesc) {
+            CUSPARSE_ERROR(cusparseSpGEMM_destroyDescr(m_spgemmDesc));
+            m_spgemmDesc = nullptr;
+        }
+
+        GPU_FREE(m_dBuffer1);
+        GPU_FREE(m_dBuffer2);
+        GPU_FREE(m_dBuffer3);
+        GPU_FREE(m_dBuffer4);
+        GPU_FREE(m_dBuffer5);
+
+        m_bufferSize1      = 0;
+        m_bufferSize2      = 0;
+        m_bufferSize3      = 0;
+        m_bufferSize4      = 0;
+        m_bufferSize5      = 0;
+        m_spgemm_ready     = false;
+        m_JtJ_nnz_expected = -1;
+
+        JtJ.release();
+        jac_trans.release();
+
+        dir.release();
+
+        is_prep_solver_called = false;
+        solve_time            = 0.0f;
+    }
 
    private:
     void create_JtJ()
     {
-       
+        // Build J^T once (allocates CSR for jac_trans and internal transpose
+        // buffer in J)
+        jac_trans = problem.jac->transpose();
+
+        // Dimension: J is (m x n), JtJ is (n x n)
+        const IndexT n = problem.jac->cols();
+
+        // If we already built SpGEMM reuse once, do not rebuild.
+        if (m_spgemm_ready) {
+            return;
+        }
+
+
+        // Create a JtJ placeholder with:
+        // - correct (n x n)
+        // - nnz = 0
+        // - device row_ptr allocated
+        // - col/val nullptr for now
+        JtJ                   = SpMatT();
+        JtJ.m_context         = problem.rx.get_context();
+        JtJ.m_block_shape     = BlockShape(1, 1);
+        JtJ.m_num_rows        = n;
+        JtJ.m_num_cols        = n;
+        JtJ.m_nnz             = 0;
+        JtJ.m_is_user_managed = false;
+        JtJ.m_op              = Op::INVALID;
+
+        CUDA_ERROR(
+            cudaMalloc((void**)&JtJ.m_d_row_ptr, (n + 1) * sizeof(IndexT)));
+        CUDA_ERROR(cudaMemset(JtJ.m_d_row_ptr, 0, (n + 1) * sizeof(IndexT)));
+
+        // We do not allocate d_col_idx/d_val yet (unknown nnz)
+        JtJ.m_d_col_idx = nullptr;
+        JtJ.m_d_val     = nullptr;
+
+        // Mark device allocation so release() logic behaves
+        JtJ.m_allocated = (JtJ.m_allocated | DEVICE);
+
+        // Create cusparse handle + placeholder CSR descriptor for JtJ
+        JtJ.init_cusparse(JtJ);
+
+        // Create SpGEMM reuse descriptor
+        CUSPARSE_ERROR(cusparseSpGEMM_createDescr(&m_spgemmDesc));
+
+
+        // We use jac_trans's handle. any handle is fine, but keep it
+        // consistent.
+        cusparseHandle_t handle = jac_trans.m_cusparse_handle;
+
+        const T alpha = T(1);
+        const T beta  = T(0);
+
+        const cusparseOperation_t opA = CUSPARSE_OPERATION_NON_TRANSPOSE;
+        const cusparseOperation_t opB = CUSPARSE_OPERATION_NON_TRANSPOSE;
+
+        // 1) workEstimation
+        CUSPARSE_ERROR(
+            cusparseSpGEMMreuse_workEstimation(handle,
+                                               opA,
+                                               opB,
+                                               jac_trans.m_spdescr,
+                                               problem.jac->m_spdescr,
+                                               JtJ.m_spdescr,
+                                               CUSPARSE_SPGEMM_DEFAULT,
+                                               m_spgemmDesc,
+                                               &m_bufferSize1,
+                                               nullptr));
+        CUDA_ERROR(cudaMalloc(&m_dBuffer1, m_bufferSize1));
+
+        CUSPARSE_ERROR(
+            cusparseSpGEMMreuse_workEstimation(handle,
+                                               opA,
+                                               opB,
+                                               jac_trans.m_spdescr,
+                                               problem.jac->m_spdescr,
+                                               JtJ.m_spdescr,
+                                               CUSPARSE_SPGEMM_DEFAULT,
+                                               m_spgemmDesc,
+                                               &m_bufferSize1,
+                                               m_dBuffer1));
+
+        // 2) nnz (symbolic) stage: computes row_ptr and nnz(C)
+        CUSPARSE_ERROR(cusparseSpGEMMreuse_nnz(handle,
+                                               opA,
+                                               opB,
+                                               jac_trans.m_spdescr,
+                                               problem.jac->m_spdescr,
+                                               JtJ.m_spdescr,
+                                               CUSPARSE_SPGEMM_DEFAULT,
+                                               m_spgemmDesc,
+                                               &m_bufferSize2,
+                                               nullptr,
+                                               &m_bufferSize3,
+                                               nullptr,
+                                               &m_bufferSize4,
+                                               nullptr));
+
+        CUDA_ERROR(cudaMalloc(&m_dBuffer2, m_bufferSize2));
+        CUDA_ERROR(cudaMalloc(&m_dBuffer3, m_bufferSize3));
+        CUDA_ERROR(cudaMalloc(&m_dBuffer4, m_bufferSize4));
+
+        CUSPARSE_ERROR(cusparseSpGEMMreuse_nnz(handle,
+                                               opA,
+                                               opB,
+                                               jac_trans.m_spdescr,
+                                               problem.jac->m_spdescr,
+                                               JtJ.m_spdescr,
+                                               CUSPARSE_SPGEMM_DEFAULT,
+                                               m_spgemmDesc,
+                                               &m_bufferSize2,
+                                               m_dBuffer2,
+                                               &m_bufferSize3,
+                                               m_dBuffer3,
+                                               &m_bufferSize4,
+                                               m_dBuffer4));
+
+        // free some early buffers
+        GPU_FREE(m_dBuffer1);
+        GPU_FREE(m_dBuffer2);
+
+
+        // Query nnz(C)
+        int64_t C_num_rows1 = 0, C_num_cols1 = 0, C_nnz1 = 0;
+        CUSPARSE_ERROR(cusparseSpMatGetSize(
+            JtJ.m_spdescr, &C_num_rows1, &C_num_cols1, &C_nnz1));
+
+
+        m_JtJ_nnz_expected = C_nnz1;
+
+        // Allocate C col/val
+        JtJ.m_nnz = static_cast<IndexT>(C_nnz1);
+
+
+        CUDA_ERROR(
+            cudaMalloc((void**)&JtJ.m_d_col_idx, JtJ.m_nnz * sizeof(IndexT)));
+        CUDA_ERROR(cudaMalloc((void**)&JtJ.m_d_val, JtJ.m_nnz * sizeof(T)));
+        CUDA_ERROR(cudaMemset(JtJ.m_d_val, 0, JtJ.m_nnz * sizeof(T)));
+
+        // Update matC descriptor pointers to the real storage
+        CUSPARSE_ERROR(cusparseCsrSetPointers(
+            JtJ.m_spdescr, JtJ.m_d_row_ptr, JtJ.m_d_col_idx, JtJ.m_d_val));
+
+
+        // Initialize cuDSS matrix now that CSR storage is complete
+        JtJ.init_cudss(JtJ);
+
+
+        // 3) copy stage (finalize reuse setup)
+        cusparseHandle_t handle2 = jac_trans.m_cusparse_handle;
+
+        CUSPARSE_ERROR(cusparseSpGEMMreuse_copy(handle2,
+                                                opA,
+                                                opB,
+                                                jac_trans.m_spdescr,
+                                                problem.jac->m_spdescr,
+                                                JtJ.m_spdescr,
+                                                CUSPARSE_SPGEMM_DEFAULT,
+                                                m_spgemmDesc,
+                                                &m_bufferSize5,
+                                                nullptr));
+        CUDA_ERROR(cudaMalloc(&m_dBuffer5, m_bufferSize5));
+
+        CUSPARSE_ERROR(cusparseSpGEMMreuse_copy(handle2,
+                                                opA,
+                                                opB,
+                                                jac_trans.m_spdescr,
+                                                problem.jac->m_spdescr,
+                                                JtJ.m_spdescr,
+                                                CUSPARSE_SPGEMM_DEFAULT,
+                                                m_spgemmDesc,
+                                                &m_bufferSize5,
+                                                m_dBuffer5));
+        GPU_FREE(m_dBuffer3);
+
+
+        m_spgemm_ready = true;
     }
+
+    void compute_JtJ()
+    {
+        // Update jac_trans numerically from current J
+        // (uses cached transpose buffer in SparseMatrix::transpose(ret))
+        problem.jac->transpose(jac_trans);
+
+        if (!m_spgemm_ready) {
+            // Fallback (should not happen if prep_solver calls create_JtJ)
+            create_JtJ();
+        }
+
+        // Sanity check nnz(JtJ) didn't change
+        if (false) {
+            int64_t r = 0, c = 0, nnz = 0;
+            CUSPARSE_ERROR(cusparseSpMatGetSize(JtJ.m_spdescr, &r, &c, &nnz));
+            if (m_JtJ_nnz_expected >= 0 && nnz != m_JtJ_nnz_expected) {
+                RXMESH_ERROR(
+                    "GaussNetwtonSolver::compute_JtJ() JtJ nnz changed ({} -> "
+                    "{}). Rebuild required.",
+                    m_JtJ_nnz_expected,
+                    nnz);
+                return;
+            }
+        }
+
+        const T alpha = T(1);
+        const T beta  = T(0);
+
+        const cusparseOperation_t opA = CUSPARSE_OPERATION_NON_TRANSPOSE;
+        const cusparseOperation_t opB = CUSPARSE_OPERATION_NON_TRANSPOSE;
+
+        // Compute: JtJ = alpha * (J^T * J) + beta * JtJ
+        CUSPARSE_ERROR(cusparseSpGEMMreuse_compute(jac_trans.m_cusparse_handle,
+                                                   opA,
+                                                   opB,
+                                                   &alpha,
+                                                   jac_trans.m_spdescr,
+                                                   problem.jac->m_spdescr,
+                                                   &beta,
+                                                   JtJ.m_spdescr,
+                                                   cuda_type<T>(),
+                                                   CUSPARSE_SPGEMM_DEFAULT,
+                                                   m_spgemmDesc));
+    }
+
+    // SpGEMM reuse state (persist across iterations)
+    bool                  m_spgemm_ready = false;
+    cusparseSpGEMMDescr_t m_spgemmDesc   = nullptr;
+    int                   m_JtJ_nnz_expected;
+
+    void*  m_dBuffer1    = nullptr;  // workEstimation
+    void*  m_dBuffer2    = nullptr;  // nnz
+    void*  m_dBuffer3    = nullptr;  // nnz
+    void*  m_dBuffer4    = nullptr;  // nnz
+    void*  m_dBuffer5    = nullptr;  // copy
+    size_t m_bufferSize1 = 0;
+    size_t m_bufferSize2 = 0;
+    size_t m_bufferSize3 = 0;
+    size_t m_bufferSize4 = 0;
+    size_t m_bufferSize5 = 0;
 };
 
 }  // namespace rxmesh
