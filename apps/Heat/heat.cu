e908df222f6ed32dbac4ab0b0e7c2309588c84ad
ahmed <a.has.mahmoud@gmail.com>
2026-01-27 16:19:44 -0500

    cli11 on Heat

--- apps/Heat/heat.cu
+++ apps/Heat/heat.cu
@@ -1,17 +1,31 @@
-#include "rxmesh/query.cuh"
-#include "rxmesh/reduce_handle.h"
 #include "rxmesh/rxmesh_static.h"
-
-#include "rxmesh/matrix/sparse_matrix.h"
+#include <CLI/CLI.hpp>
 
 using namespace rxmesh;
 
 
 int main(int argc, char** argv)
 {
-    rx_init(0);
+    CLI::App app{"Heat"};
+    
+    std::string mesh_path = STRINGIFY(INPUT_DIR) "sphere3.obj";
+    uint32_t device_id = 0;
+    
+    app.add_option("-i,--input", mesh_path, "Input OBJ mesh file")
+        ->default_val(std::string(STRINGIFY(INPUT_DIR) "sphere3.obj"));
+    
+    app.add_option("-d,--device_id", device_id, "GPU device ID")
+        ->default_val(0u);
 
-    RXMeshStatic rx(STRINGIFY(INPUT_DIR) "sphere3.obj");
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError& e) {
+        return app.exit(e);
+    }
+
+    rx_init(device_id);
+
+    RXMeshStatic rx(mesh_path);
 
 #if USE_POLYSCOPE
     polyscope::show();
