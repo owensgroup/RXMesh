139c75385bb0b5e492c495b14c24de71e37ce748
ahmed <a.has.mahmoud@gmail.com>
2026-08-22 14:10:52 -0400

    move hash function inline device impl to .h file

--- include/rxmesh/hash_functions.h
+++ include/rxmesh/hash_functions.h
@@ -115,13 +115,60 @@ struct MurmurHash3_32
     MurmurHash3_32& operator=(MurmurHash3_32&&)      = default;
     ~MurmurHash3_32()                                = default;
 
-    uint32_t __host__ __device__ operator()(Key const& key) const noexcept;
+    uint32_t __host__ __device__ operator()(Key const& key) const noexcept
+    {
+        constexpr int        len     = sizeof(Key);
+        const uint8_t* const data    = (const uint8_t*)&key;
+        constexpr int        nblocks = len / 4;
+
+        uint32_t              h1     = m_seed;
+        constexpr uint32_t    c1     = 0xcc9e2d51;
+        constexpr uint32_t    c2     = 0x1b873593;
+        const uint32_t* const blocks = (const uint32_t*)(data + nblocks * 4);
+        for (int i = -nblocks; i; i++) {
+            uint32_t k1 = blocks[i];
+            k1 *= c1;
+            k1 = rotl32(k1, 15);
+            k1 *= c2;
+            h1 ^= k1;
+            h1 = rotl32(h1, 13);
+            h1 = h1 * 5 + 0xe6546b64;
+        }
+        const uint8_t* tail = (const uint8_t*)(data + nblocks * 4);
+        uint32_t       k1   = 0;
+        switch (len & 3) {
+            case 3:
+                k1 ^= tail[2] << 16;
+            case 2:
+                k1 ^= tail[1] << 8;
+            case 1:
+                k1 ^= tail[0];
+                k1 *= c1;
+                k1 = rotl32(k1, 15);
+                k1 *= c2;
+                h1 ^= k1;
+        };
+        h1 ^= len;
+        h1 = fmix32(h1);
+        return h1;
+    }
 
    private:
     constexpr __host__ __device__ uint32_t rotl32(uint32_t x,
-                                                  int8_t   r) const noexcept;
-    constexpr __host__ __device__ uint32_t fmix32(uint32_t h) const noexcept;
-    uint32_t                               m_seed;
+                                                  int8_t   r) const noexcept
+    {
+        return (x << r) | (x >> (32 - r));
+    }
+    constexpr __host__ __device__ uint32_t fmix32(uint32_t h) const noexcept
+    {
+        h ^= h >> 16;
+        h *= 0x85ebca6b;
+        h ^= h >> 13;
+        h *= 0xc2b2ae35;
+        h ^= h >> 16;
+        return h;
+    }
+    uint32_t m_seed;
 };
 
 
@@ -136,7 +183,15 @@ struct hash16_xm2
     ~hash16_xm2()                            = default;
 
     constexpr uint16_t __host__ __device__
-    operator()(uint16_t key) const noexcept;
+    operator()(uint16_t key) const noexcept
+    {
+        key ^= key >> 8;
+        key *= 0x88b5U;
+        key ^= key >> 7;
+        key *= 0xdb2dU;
+        key ^= key >> 9;
+        return key;
+    }
 };
 
 // hashing 64bit to 32bit
@@ -152,7 +207,10 @@ struct Hash64To32XOR
     {
     }
 
-    constexpr uint32_t __host__ __device__ operator()(const uint64_t k) const;
+    constexpr uint32_t __host__ __device__ operator()(const uint64_t k) const
+    {
+        return (uint32_t)(k ^ (k >> 32) ^ seed);
+    }
 };
 
 template <typename HashT, typename RNG>
